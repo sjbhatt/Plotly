a59b75e3a4fc7f4ae722556807ad216093534088
Guru <sjb.bhatt@gmail.com>
2019-06-09 13:45:47 -0700

    Heroku files added

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -13,6 +13,20 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+
+  var url = "/metadata/" + sample ;
+  d3.json(url).then(function(response) {
+    // console.log (response);
+    // console.log (Object.entries(response));
+
+    var metadataPanel = d3.select("#sample-metadata");
+    metadataPanel.html("");
+    Object.entries(response).forEach(function ([key, value]) {
+      var row = metadataPanel.append("p");
+      row.text(`${key} : ${value}`);
+      // row.append("hr")
+  });
+});
 }
 
 function buildCharts(sample) {
@@ -24,6 +38,94 @@ function buildCharts(sample) {
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+  var url = "/samples/" + sample ;
+  d3.json(url).then(function(response) {
+    var raw_data = [response];
+    var samplevalues = raw_data[0].sample_values;
+    var otuids = raw_data[0].otu_ids;
+    var otulabels = raw_data[0].otu_labels
+    // console.log(`Sample Values ${samplevalues}`);
+    // console.log(`Otu IDs ${otuids}`);
+    // console.log(`Otu Labels ${otulabels}`);
+
+    var sample_data =[];
+
+    for (var i = 0; i < samplevalues.length; i++) {
+      var data = { "sample_values" : samplevalues[i],
+          "otu_ids" : otuids[i],
+          "otu_labels" : otulabels[i]
+        }
+        sample_data.push(data);
+    }
+
+    // console.log(sample_data);
+
+    sample_data.sort(function (a, b) {
+      return b.sample_values - a.sample_values
+    });
+    // console.log(sample_data);
+    // console.log(sample_data[0].sample_values);
+    // console.log(sample_data[0].otu_ids);
+    // console.log(sample_data[0].otu_labels);
+
+    var top_10 = sample_data.slice(0,10);
+    // console.log(top_10);
+
+    var pie_labels = top_10.map(label => label.otu_ids);
+    // console.log(pie_labels);
+    var pie_values = top_10.map(value => value.sample_values);
+    // console.log(pie_values);
+    var pie_hovertexts = top_10.map(hovertext => hovertext.otu_labels);
+    // console.log(pie_hovertexts);
+
+    var pie_trace = {
+      labels: pie_labels,
+      values: pie_values,
+      type: 'pie',
+      hovertext : pie_hovertexts
+      };
+  
+    var data = [pie_trace];
+  
+    var layout = {
+      height: 500,
+      width: 500,
+      title: '<b> Belly Button Pie Chart for Top 10 Samples </b>'
+  };
+  
+  Plotly.newPlot("pie", data, layout);
+
+  var bubble_trace = {
+    x: otuids,
+    y: samplevalues,
+    mode: 'markers',
+    marker: {
+      size: samplevalues,
+      sizeref: 1.5,
+      color: otuids,
+      colorscale: 'Portland',
+    },
+    text: otulabels
+  };
+  
+  var data = [bubble_trace];
+  
+  var layout = {
+    title:"<b> Belly Button Bubble Chart </b>",
+    xaxis: { title: 'OTU ID'},
+    yaxis: { title: 'Sample Value'},
+    height: 800,
+    width: 1200
+  };
+  
+  Plotly.newPlot('bubble', data, layout);
+
+});
+
+
+  
+
 }
 
 function init() {
